6d8904407be31c6dc46ec3c0620d9126f3c55ad8
Hakeem Bux <h.bux@uea.ac.uk>
2017-11-03 12:27:40 +0000

    Adding Programming2_Assignment1

---
+++ Inventory.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Inventory.h
+ * Author: Hakeem
+ *
+ * Created on 03 November 2017, 12:08
+ */
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INVENTORY_H */
+
---
+++ Sales.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Sales.h
+ * Author: Hakeem
+ *
+ * Created on 03 November 2017, 12:07
+ */
+
+#ifndef SALES_H
+#define SALES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SALES_H */
+
